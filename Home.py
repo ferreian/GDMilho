8167142320477969412abb212d444fb9cbff35f7
ferreian <150438244+ferreian@users.noreply.github.com>
2024-08-06 14:31:13 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -74,7 +74,7 @@ if uploaded_file is not None:
         df_filtered = df_filtered[df_filtered['Time'].isin(selected_time)]
 
     # Criar tabs
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Dados Carregados", "Yield", "LxL e Vitrines", "Densidades", "População vs Rendimento", "Head to Head"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Dados Carregados", "Yield", "LxL e Vitrines", "População vs Rendimento", "Head to Head"])
 
     with tab1:
         st.write("Dados do Excel carregados:")
@@ -95,146 +95,178 @@ if uploaded_file is not None:
             st.write("O arquivo Excel não contém todas as colunas especificadas.")
     
     with tab3:
-        # Filtrar dados onde Ensaio não contém "densidade"
-        if 'Ensaio' in df_filtered.columns:
-            df_lxl_vitrines = df_filtered[~df_filtered['Ensaio'].str.contains('densidade', case=False, na=False)]
+        # Reorganizar colunas para colocar PR Maior ao lado de Produção (sc/há)
+        cols_order = [
+            'Híbridos', '% Acamadas', '% Quebradas', '% Dominadas', 'Alt Planta', 
+            'Alt Espiga', 'Pop Final', 'Colheita', 'Produção (sc/há)', 'PR Maior', 'PR Maior Label', 'Município', 
+            'UF', 'Altitude', 'Ensaio', 'Época', 'Investimento', 'Time'
+        ]
+        df_lxl_vitrines = df_filtered[cols_order]
+        
+        st.write("LxL e Vitrines:")
+        st.dataframe(df_lxl_vitrines, width=1500)  # Ajuste o valor de width conforme necessário
+        
+        # Gerar sumário das principais estatísticas descritivas
+        if 'Produção (sc/há)' in df_lxl_vitrines.columns:
+            summary_stats = df_lxl_vitrines['Produção (sc/há)'].describe()
+
+            # Calcular estatísticas descritivas para os híbridos específicos
+            hybrids = ['9801 VIP3', '9705 VIP3', '9504 VIP3', 'K1627 VIP3', 'SZE6192 VIP3']
+            stats_hybrids = {hybrid: df_lxl_vitrines[df_lxl_vitrines['Híbridos'] == hybrid]['Produção (sc/há)'].describe() for hybrid in hybrids}
             
-            # Reorganizar colunas para colocar PR Maior ao lado de Produção (sc/há)
-            cols_order = [
-                'Híbridos', '% Acamadas', '% Quebradas', '% Dominadas', 'Alt Planta', 
-                'Alt Espiga', 'Pop Final', 'Colheita', 'Produção (sc/há)', 'PR Maior', 'PR Maior Label', 'Município', 
-                'UF', 'Altitude', 'Ensaio', 'Época', 'Investimento', 'Time'
-            ]
-            df_lxl_vitrines = df_lxl_vitrines[cols_order]
+            # Combinar as estatísticas gerais com as dos híbridos específicos
+            combined_stats = summary_stats.to_frame(name='Geral')
+            for hybrid, stats in stats_hybrids.items():
+                combined_stats[hybrid] = stats
             
-            st.write("LxL e Vitrines:")
-            st.dataframe(df_lxl_vitrines, width=1500)  # Ajuste o valor de width conforme necessário
+            st.write("Sumário das Principais Estatísticas Descritivas:")
+            st.dataframe(combined_stats)
             
-            # Gerar sumário das principais estatísticas descritivas
-            if 'Produção (sc/há)' in df_lxl_vitrines.columns:
-                summary_stats = df_lxl_vitrines['Produção (sc/há)'].describe()
+            # Calcular valores médios por híbrido para Produção (sc/há)
+            mean_production_by_hybrid = df_lxl_vitrines.groupby('Híbridos')['Produção (sc/há)'].mean().reset_index()
+            mean_production_by_hybrid = mean_production_by_hybrid.sort_values(by='Produção (sc/há)', ascending=False)
+            
+            # Definir cores para os híbridos específicos
+            colors = {
+                '9705 VIP3': 'darkblue',
+                '9801 VIP3': 'darkblue',
+                '9504 VIP3': 'darkblue',
+                'K1627 VIP3': '#FF8C00',  # Laranja escuro
+                'SZE6192 VIP3': 'green'
+            }
+            mean_production_by_hybrid['Cor'] = mean_production_by_hybrid['Híbridos'].map(colors).fillna('gray')
+            
+            # Adicionar rótulos sem a diferença para o valor anterior
+            mean_production_by_hybrid['Label'] = mean_production_by_hybrid['Produção (sc/há)'].round(1).astype(str)
 
-                # Calcular estatísticas descritivas para os híbridos específicos
-                hybrids = ['9801 VIP3', '9705 VIP3', '9504 VIP3', 'K1627 VIP3', 'SZE6192 VIP3']
-                stats_hybrids = {hybrid: df_lxl_vitrines[df_lxl_vitrines['Híbridos'] == hybrid]['Produção (sc/há)'].describe() for hybrid in hybrids}
-                
-                # Combinar as estatísticas gerais com as dos híbridos específicos
-                combined_stats = summary_stats.to_frame(name='Geral')
-                for hybrid, stats in stats_hybrids.items():
-                    combined_stats[hybrid] = stats
-                
-                st.write("Sumário das Principais Estatísticas Descritivas:")
-                st.dataframe(combined_stats)
-                
-                # Calcular valores médios por híbrido para Produção (sc/há)
-                mean_production_by_hybrid = df_lxl_vitrines.groupby('Híbridos')['Produção (sc/há)'].mean().reset_index()
-                mean_production_by_hybrid = mean_production_by_hybrid.sort_values(by='Produção (sc/há)', ascending=False)
-                
-                # Definir cores para os híbridos específicos
-                colors = {
-                    '9705 VIP3': 'darkblue',
-                    '9801 VIP3': 'darkblue',
-                    '9504 VIP3': 'darkblue',
-                    'K1627 VIP3': '#FF8C00',  # Laranja escuro
-                    'SZE6192 VIP3': 'green'
-                }
-                mean_production_by_hybrid['Cor'] = mean_production_by_hybrid['Híbridos'].map(colors).fillna('gray')
-                
-                # Adicionar rótulos sem a diferença para o valor anterior
-                mean_production_by_hybrid['Label'] = mean_production_by_hybrid['Produção (sc/há)'].round(1).astype(str)
+            # Calcular a média de produção
+            media_producao = mean_production_by_hybrid['Produção (sc/há)'].mean()
 
-                # Calcular a média de produção
-                media_producao = mean_production_by_hybrid['Produção (sc/há)'].mean()
+            # Criar gráfico de barras para Produção (sc/há)
+            fig1 = px.bar(mean_production_by_hybrid, x='Híbridos', y='Produção (sc/há)', 
+                          title='Produção Média por Híbrido', 
+                          labels={'Híbridos': 'Híbridos', 'Produção (sc/há)': 'Produção Média (sc/há)'},
+                          color='Cor',
+                          color_discrete_map='identity',
+                          text='Label')
+            
+            # Configurar rótulos de dados em branco
+            fig1.update_traces(textfont_color='white')
+            
+            # Adicionar linha tracejada da média em vermelho
+            fig1.add_trace(go.Scatter(
+                x=mean_production_by_hybrid['Híbridos'],
+                y=[media_producao] * len(mean_production_by_hybrid),
+                mode="lines",
+                name="Média",
+                line=dict(color="red", dash="dash")
+            ))
 
-                # Criar gráfico de barras para Produção (sc/há)
-                fig1 = px.bar(mean_production_by_hybrid, x='Híbridos', y='Produção (sc/há)', 
-                              title='Produção Média por Híbrido', 
-                              labels={'Híbridos': 'Híbridos', 'Produção (sc/há)': 'Produção Média (sc/há)'},
-                              color='Cor',
-                              color_discrete_map='identity',
-                              text='Label')
-                
-                # Configurar rótulos de dados em branco
-                fig1.update_traces(textfont_color='white')
-                
-                # Adicionar linha tracejada da média em vermelho
-                fig1.add_trace(go.Scatter(
-                    x=mean_production_by_hybrid['Híbridos'],
-                    y=[media_producao] * len(mean_production_by_hybrid),
-                    mode="lines",
-                    name="Média",
-                    line=dict(color="red", dash="dash")
-                ))
+            # Adicionar anotação para a média
+            fig1.add_annotation(
+                x=mean_production_by_hybrid['Híbridos'].iloc[-1],
+                y=media_producao,
+                text=f"Média: {media_producao:.1f}",
+                showarrow=True,
+                arrowhead=1,
+                ax=0,
+                ay=-40
+            )
+            
+            st.plotly_chart(fig1)
 
-                # Adicionar anotação para a média
-                fig1.add_annotation(
-                    x=mean_production_by_hybrid['Híbridos'].iloc[-1],
-                    y=media_producao,
-                    text=f"Média: {media_producao:.1f}",
-                    showarrow=True,
-                    arrowhead=1,
-                    ax=0,
-                    ay=-40
-                )
-                
-                st.plotly_chart(fig1)
+            # Agrupar dados por UF, Híbridos e Time para evitar duplicados e calcular a média
+            grouped_data = df_lxl_vitrines.groupby(['UF', 'Híbridos', 'Time'], as_index=False)['PR Maior'].mean()
+            grouped_data['PR Maior Label'] = grouped_data['PR Maior'].round(1).astype(str)
+            
+            # Criar gráfico de calor para PR Maior com UF no eixo y e Híbridos no eixo x
+            heatmap_data = grouped_data.pivot_table(index='UF', columns='Híbridos', values='PR Maior', aggfunc='mean')
+            heatmap_data_labels = grouped_data.pivot_table(index='UF', columns='Híbridos', values='PR Maior Label', aggfunc='first')
+            fig2 = px.imshow(heatmap_data, aspect="auto", color_continuous_scale=['lightcoral', 'lightgreen', 'darkgreen'], 
+                             labels={'color': 'PR Maior'}, title='Mapa de Calor da Produção Relativa (PR Maior)')
+            fig2.update_traces(text=heatmap_data_labels, texttemplate="%{text}", textfont_color='white')
+            st.plotly_chart(fig2)
 
-                # Agrupar dados por UF, Híbridos e Time para evitar duplicados e calcular a média
-                grouped_data = df_lxl_vitrines.groupby(['UF', 'Híbridos', 'Time'], as_index=False)['PR Maior'].mean()
-                grouped_data['PR Maior Label'] = grouped_data['PR Maior'].round(1).astype(str)
-                
-                # Criar gráfico de calor para PR Maior com UF no eixo y e Híbridos no eixo x
-                heatmap_data = grouped_data.pivot_table(index='UF', columns='Híbridos', values='PR Maior', aggfunc='mean')
-                heatmap_data_labels = grouped_data.pivot_table(index='UF', columns='Híbridos', values='PR Maior Label', aggfunc='first')
-                fig2 = px.imshow(heatmap_data, aspect="auto", color_continuous_scale=['lightcoral', 'lightgreen', 'darkgreen'], 
-                                 labels={'color': 'PR Maior'}, title='Mapa de Calor da Produção Relativa (PR Maior)')
-                fig2.update_traces(text=heatmap_data_labels, texttemplate="%{text}", textfont_color='white')
-                st.plotly_chart(fig2)
+            # Filtrar dados para os híbridos específicos
+            selected_hybrids = ['9801 VIP3', '9705 VIP3', '9504 VIP3', 'SZE6192 VIP3', 'K1627 VIP3']
+            df_selected_hybrids = df_filtered[df_filtered['Híbridos'].isin(selected_hybrids)]
+            
+            # Agrupar dados por UF, Híbridos e Time para evitar duplicados e calcular a média
+            grouped_data_selected = df_selected_hybrids.groupby(['UF', 'Híbridos', 'Time'], as_index=False)['PR Maior'].mean()
+            grouped_data_selected['PR Maior Label'] = grouped_data_selected['PR Maior'].round(1).astype(str)
+            
+            # Criar gráfico de calor para PR Maior com UF no eixo y e Híbridos no eixo x
+            heatmap_data_selected = grouped_data_selected.pivot_table(index='UF', columns='Híbridos', values='PR Maior', aggfunc='mean')
+            heatmap_data_labels_selected = grouped_data_selected.pivot_table(index='UF', columns='Híbridos', values='PR Maior Label', aggfunc='first')
+            fig3 = px.imshow(heatmap_data_selected, aspect="auto", color_continuous_scale=['lightcoral', 'lightgreen', 'darkgreen'], 
+                             labels={'color': 'PR Maior'}, title='Mapa de Calor da Produção Relativa (PR Maior) para Híbridos Selecionados')
+            fig3.update_traces(text=heatmap_data_labels_selected, texttemplate="%{text}", textfont_color='white')
+            st.plotly_chart(fig3)
 
-                # Filtrar dados para os híbridos específicos
-                selected_hybrids = ['9801 VIP3', '9705 VIP3', '9504 VIP3', 'SZE6192 VIP3', 'K1627 VIP3']
-                df_selected_hybrids = df_filtered[df_filtered['Híbridos'].isin(selected_hybrids)]
-                
-                # Agrupar dados por UF, Híbridos e Time para evitar duplicados e calcular a média
-                grouped_data_selected = df_selected_hybrids.groupby(['UF', 'Híbridos', 'Time'], as_index=False)['PR Maior'].mean()
-                grouped_data_selected['PR Maior Label'] = grouped_data_selected['PR Maior'].round(1).astype(str)
-                
-                # Criar gráfico de calor para PR Maior com UF no eixo y e Híbridos no eixo x
-                heatmap_data_selected = grouped_data_selected.pivot_table(index='UF', columns='Híbridos', values='PR Maior', aggfunc='mean')
-                heatmap_data_labels_selected = grouped_data_selected.pivot_table(index='UF', columns='Híbridos', values='PR Maior Label', aggfunc='first')
-                fig3 = px.imshow(heatmap_data_selected, aspect="auto", color_continuous_scale=['lightcoral', 'lightgreen', 'darkgreen'], 
-                                 labels={'color': 'PR Maior'}, title='Mapa de Calor da Produção Relativa (PR Maior) para Híbridos Selecionados')
-                fig3.update_traces(text=heatmap_data_labels_selected, texttemplate="%{text}", textfont_color='white')
-                st.plotly_chart(fig3)
-
-            else:
-                st.write("A coluna 'Produção (sc/há)' não está presente no DataFrame.")
         else:
-            st.write("A coluna 'Ensaio' não está presente no arquivo Excel.")
+            st.write("A coluna 'Produção (sc/há)' não está presente no DataFrame.")
     
     with tab4:
-        # Filtrar dados onde Ensaio contém "densidade"
-        if 'Ensaio' in df_filtered.columns:
-            df_densidades = df_filtered[df_filtered['Ensaio'].str.contains('densidade', case=False, na=False)]
-            st.write("Densidades:")
-            st.dataframe(df_densidades, width=1500)  # Ajuste o valor de width conforme necessário
-        else:
-            st.write("A coluna 'Ensaio' não está presente no arquivo Excel.")
-    
-    with tab5:
         st.write("População vs Rendimento:")
         
-        # Categorizar a população em 4 faixas
-        df_filtered['População Categoria'] = pd.cut(df_filtered['Pop Final'], bins=4, labels=['Baixa', 'Média', 'Alta', 'Muito Alta'])
+        # Garantir que o valor máximo da Pop Final seja maior que 85000
+        max_pop_final = df_filtered['Pop Final'].max()
+        if max_pop_final <= 85000:
+            max_pop_final = 85001
+        
+        # Categorizar a população em 5 faixas
+        pop_bins = [0, 55000, 65000, 75000, 85000, max_pop_final]
+        pop_labels = ['Menor que 55000', '55001 a 65000', '65001 a 75000', '75001 a 85000', 'Maior que 85000']
+        df_filtered['População Categoria'] = pd.cut(df_filtered['Pop Final'], bins=pop_bins, labels=pop_labels, include_lowest=True)
+        
+        # Transformar a coluna em uma categoria ordenada explicitamente
+        df_filtered['População Categoria'] = pd.Categorical(df_filtered['População Categoria'], categories=pop_labels, ordered=True)
 
         # Criar box plot para População Categoria no eixo x e Produção (sc/há) no eixo y
-        fig4 = px.box(df_filtered, x='População Categoria', y='Produção (sc/há)', color='Híbridos',
-                      title='Box Plot de Produção por Faixa de População',
-                      labels={'População Categoria': 'Faixa de População', 'Produção (sc/há)': 'Produção (sc/há)'})
-        
+        fig4 = go.Figure()
+
+        for hibrido in df_filtered['Híbridos'].unique():
+            df_hibrido = df_filtered[df_filtered['Híbridos'] == hibrido]
+            fig4.add_trace(go.Box(
+                y=df_hibrido['Produção (sc/há)'],
+                x=df_hibrido['População Categoria'],
+                name=hibrido,
+                marker_color='white'
+            ))
+            # Adicionar pontos indicando a média
+            mean_values = df_hibrido.groupby('População Categoria')['Produção (sc/há)'].mean().reset_index()
+            fig4.add_trace(go.Scatter(
+                x=mean_values['População Categoria'],
+                y=mean_values['Produção (sc/há)'],
+                mode='markers+text',
+                marker=dict(color='black', size=10),
+                text=mean_values['Produção (sc/há)'].round(1).astype(str),
+                textposition="top center",
+                name=f'Média {hibrido}'
+            ))
+
+            # Adicionar pontos indicando a mediana
+            median_values = df_hibrido.groupby('População Categoria')['Produção (sc/há)'].median().reset_index()
+            fig4.add_trace(go.Scatter(
+                x=median_values['População Categoria'],
+                y=median_values['Produção (sc/há)'],
+                mode='markers+text',
+                marker=dict(color='red', size=10),
+                text=median_values['Produção (sc/há)'].round(1).astype(str),
+                textposition="bottom center",
+                name=f'Mediana {hibrido}'
+            ))
+
+        fig4.update_layout(
+            title='Box Plot de Produção por Faixa de População',
+            xaxis_title='Faixa de População',
+            yaxis_title='Produção (sc/há)',
+            showlegend=True
+        )
+
         st.plotly_chart(fig4)
     
-    with tab6:
+    with tab5:
         st.write("Head to Head")
         
         col1, col2, col3 = st.columns([1, 0.1, 1])
