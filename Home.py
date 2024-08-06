0c603d71074525c326ee5281f1a5423899b01d63
ferreian <150438244+ferreian@users.noreply.github.com>
2024-08-06 11:29:02 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -74,7 +74,7 @@ if uploaded_file is not None:
         df_filtered = df_filtered[df_filtered['Time'].isin(selected_time)]
 
     # Criar tabs
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Dados Carregados", "Yield", "LxL e Vitrines", "Densidades", "População vs Rendimento"])
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Dados Carregados", "Yield", "LxL e Vitrines", "Densidades", "População vs Rendimento", "Head to Head"])
 
     with tab1:
         st.write("Dados do Excel carregados:")
@@ -179,13 +179,13 @@ if uploaded_file is not None:
                 
                 st.plotly_chart(fig1)
 
-                # Agrupar dados por UF e Híbridos para evitar duplicados
-                grouped_data = df_lxl_vitrines.groupby(['UF', 'Híbridos', 'Time'])['PR Maior'].mean().reset_index()
+                # Agrupar dados por UF, Híbridos e Time para evitar duplicados e calcular a média
+                grouped_data = df_lxl_vitrines.groupby(['UF', 'Híbridos', 'Time'], as_index=False)['PR Maior'].mean()
                 grouped_data['PR Maior Label'] = grouped_data['PR Maior'].round(1).astype(str)
                 
                 # Criar gráfico de calor para PR Maior com UF no eixo y e Híbridos no eixo x
-                heatmap_data = grouped_data.pivot(index='UF', columns='Híbridos', values='PR Maior')
-                heatmap_data_labels = grouped_data.pivot(index='UF', columns='Híbridos', values='PR Maior Label')
+                heatmap_data = grouped_data.pivot_table(index='UF', columns='Híbridos', values='PR Maior', aggfunc='mean')
+                heatmap_data_labels = grouped_data.pivot_table(index='UF', columns='Híbridos', values='PR Maior Label', aggfunc='first')
                 fig2 = px.imshow(heatmap_data, aspect="auto", color_continuous_scale=['lightcoral', 'lightgreen', 'darkgreen'], 
                                  labels={'color': 'PR Maior'}, title='Mapa de Calor da Produção Relativa (PR Maior)')
                 fig2.update_traces(text=heatmap_data_labels, texttemplate="%{text}", textfont_color='white')
@@ -195,13 +195,13 @@ if uploaded_file is not None:
                 selected_hybrids = ['9801 VIP3', '9705 VIP3', '9504 VIP3', 'SZE6192 VIP3', 'K1627 VIP3']
                 df_selected_hybrids = df_filtered[df_filtered['Híbridos'].isin(selected_hybrids)]
                 
-                # Agrupar dados por UF e Híbridos para evitar duplicados
-                grouped_data_selected = df_selected_hybrids.groupby(['UF', 'Híbridos', 'Time'])['PR Maior'].mean().reset_index()
+                # Agrupar dados por UF, Híbridos e Time para evitar duplicados e calcular a média
+                grouped_data_selected = df_selected_hybrids.groupby(['UF', 'Híbridos', 'Time'], as_index=False)['PR Maior'].mean()
                 grouped_data_selected['PR Maior Label'] = grouped_data_selected['PR Maior'].round(1).astype(str)
                 
                 # Criar gráfico de calor para PR Maior com UF no eixo y e Híbridos no eixo x
-                heatmap_data_selected = grouped_data_selected.pivot(index='UF', columns='Híbridos', values='PR Maior')
-                heatmap_data_labels_selected = grouped_data_selected.pivot(index='UF', columns='Híbridos', values='PR Maior Label')
+                heatmap_data_selected = grouped_data_selected.pivot_table(index='UF', columns='Híbridos', values='PR Maior', aggfunc='mean')
+                heatmap_data_labels_selected = grouped_data_selected.pivot_table(index='UF', columns='Híbridos', values='PR Maior Label', aggfunc='first')
                 fig3 = px.imshow(heatmap_data_selected, aspect="auto", color_continuous_scale=['lightcoral', 'lightgreen', 'darkgreen'], 
                                  labels={'color': 'PR Maior'}, title='Mapa de Calor da Produção Relativa (PR Maior) para Híbridos Selecionados')
                 fig3.update_traces(text=heatmap_data_labels_selected, texttemplate="%{text}", textfont_color='white')
@@ -230,3 +230,83 @@ if uploaded_file is not None:
                       labels={'Pop Final': 'População Final', 'Produção (sc/há)': 'Produção (sc/há)'})
         
         st.plotly_chart(fig4)
+    
+    with tab6:
+        st.write("Head to Head")
+        
+        col1, col2, col3 = st.columns([1, 0.1, 1])
+        
+        head_options = ['9801 VIP3', '9705 VIP3', '9504 VIP3']
+        
+        with col1:
+            selected_head_hibrido = st.selectbox('Selecione o Híbrido para Head', options=head_options)
+        
+        with col2:
+            st.markdown("<h1 style='text-align: center;'>VS</h1>", unsafe_allow_html=True)
+        
+        with col3:
+            selected_check_hibrido = st.selectbox('Selecione o Híbrido para Check', options=hibrido_options)
+        
+        if selected_head_hibrido and selected_check_hibrido:
+            df_head = df_filtered[df_filtered['Híbridos'] == selected_head_hibrido]
+            df_check = df_filtered[df_filtered['Híbridos'] == selected_check_hibrido]
+            
+            # Calcular a diferença média de produção por município
+            df_head_avg = df_head.groupby('Município')['Produção (sc/há)'].mean().reset_index()
+            df_check_avg = df_check.groupby('Município')['Produção (sc/há)'].mean().reset_index()
+            
+            df_comparison = pd.merge(df_head_avg, df_check_avg, on='Município', suffixes=('_head', '_check'))
+            df_comparison['Diferença'] = df_comparison['Produção (sc/há)_head'] - df_comparison['Produção (sc/há)_check']
+            
+            # Adicionar coluna para cores
+            df_comparison['Cor'] = df_comparison['Diferença'].apply(lambda x: 'green' if x > 1 else ('red' if x < -1 else 'orange'))
+            
+            # Ajustar rótulo para uma casa decimal
+            df_comparison['Diferença Label'] = df_comparison['Diferença'].round(1).astype(str)
+
+            # Contar vitórias, derrotas e empates
+            vitorias = df_comparison[df_comparison['Diferença'] > 1].shape[0]
+            derrotas = df_comparison[df_comparison['Diferença'] < -1].shape[0]
+            empates = df_comparison[(df_comparison['Diferença'] <= 1) & (df_comparison['Diferença'] >= -1)].shape[0]
+
+            # Calcular a maior vitória, a média das vitórias, a maior derrota e a média das derrotas
+            maior_vitoria = df_comparison[df_comparison['Diferença'] > 1]['Diferença'].max()
+            media_vitorias = df_comparison[df_comparison['Diferença'] > 1]['Diferença'].mean()
+            maior_derrota = df_comparison[df_comparison['Diferença'] < -1]['Diferença'].min()
+            media_derrotas = df_comparison[df_comparison['Diferença'] < -1]['Diferença'].mean()
+            
+            # Formatar métricas para exibir "-" quando não houver valores
+            maior_vitoria = round(maior_vitoria, 1) if not pd.isna(maior_vitoria) else "-"
+            media_vitorias = round(media_vitorias, 1) if not pd.isna(media_vitorias) else "-"
+            maior_derrota = round(maior_derrota, 1) if not pd.isna(maior_derrota) else "-"
+            media_derrotas = round(media_derrotas, 1) if not pd.isna(media_derrotas) else "-"
+            
+            # Calcular o total de locais
+            total_locais = df_comparison.shape[0]
+            
+            # Mostrar cards
+            col4, col5, col6, col7 = st.columns(4)
+            with col4:
+                st.metric(label="Número de Vitórias", value=vitorias)
+                st.metric(label="Maior Vitória", value=maior_vitoria)
+                st.metric(label="Média das Vitórias", value=media_vitorias)
+            with col5:
+                st.metric(label="Número de Empates", value=empates)
+            with col6:
+                st.metric(label="Número de Derrotas", value=derrotas)
+                st.metric(label="Maior Derrota", value=maior_derrota)
+                st.metric(label="Média das Derrotas", value=media_derrotas)
+            with col7:
+                st.metric(label="Total de Locais", value=total_locais)
+            
+            # Mostrar gráfico
+            fig_head = px.bar(df_comparison, x='Município', y='Diferença', 
+                              title='Diferença de Produção por Local',
+                              labels={'Diferença': 'Diferença de Produção (sc/há)', 'Município': 'Município'},
+                              color='Cor',
+                              color_discrete_map={'green': 'green', 'red': 'red', 'orange': 'orange'},
+                              text='Diferença Label')
+            
+            fig_head.update_layout(showlegend=False)  # Remover a legenda
+            
+            st.plotly_chart(fig_head)
